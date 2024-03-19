0246d1673fc72b899a0ccb0956db2f191f7984ba
ckk914 <ckk9114@naver.com>
2024-03-19 23:53:37 +0900

    js 비교 연산 테스트

---
+++ 1. 자바스크립트 기초/4. 비교연산자.js
@@ -0,0 +1,19 @@
+
+var a=5;
+var b='5';
+
+// 3개 써야함
+console.log(a===b);  //false
+console.log(a!==b);  //true
+
+//전부 false
+console.log('===========');
+console.log('0'==='');
+console.log(0==='');
+console.log('0'===0);
+console.log(false === 'false');
+console.log(false ==='0');
+
+//대소 비교   97   65
+console.log('a'>'A');  //true
+console.log('강'<'황');
