9507ee74cf7de8b2230d20e919aae402e6c7d03e
ckk914 <ckk9114@naver.com>
2024-03-22 14:06:30 +0900

    중첩 loop 테스트

--- 2.제어문/13.중첩loop_quiz.js
+++ 2.제어문/13.중첩loop_quiz.js
@@ -1,12 +1,10 @@
 var num1 = +prompt(`한변 입력1:`);
 var num2 = +prompt(`한변 입력2:`);
 var total = ``;
-for(var i=0;i<num1;i++)
-{
-  for(var j=0;j<num2;j++)
-  {
-   total+=`*   `;
+for (var i = 0; i < num1; i++) {
+  for (var j = 0; j < num2; j++) {
+    total += `*   `;
   }
-   total+=`\n`;
+  total += `\n`;
 }
 alert(total);
