22d795c3b81bc799f93b232be56311d802bd460f
ckk914 <ckk9114@naver.com>
2024-03-27 17:44:39 +0900

    콜백함수-문제 연습, 풀이 체크

--- 8.ES6/7.quiz2.js
+++ 8.ES6/7.quiz2.js
@@ -37,9 +37,8 @@ const userList = [
   },
 ];
 
-//
+//min search
 function min(userList,condition){
-
   let minValue = userList[0][condition];
   // console.log(minValue);
   for(const a of userList){
@@ -47,7 +46,7 @@ function min(userList,condition){
 }
 return minValue;
 }
-
+// find
 function find(condition){
   // const findArray = [];
   // console.log(condition(user));
@@ -55,14 +54,25 @@ for(const a of userList){
   if(condition(a)) return a;
 }
 }
+// some
 function some(condition){
   const filteredArray = [];
   for(const a of userList){
-    if(condition(a)) filteredArray.push(a);
+    if(condition(a)) return true;  //있는지 검사
+    // if(condition(a)) filteredArray.push(a); //맞는 값 반환하기.
   }
-  return filteredArray;
+  return false;
+  // return filteredArray;   //맞는값 반환하기2
 }
-
+//every
+function every(callback) {
+  for (const user of userList) {
+    if (!callback(user)) {
+      return false;
+    }
+  }
+  return true;
+}
 //
 console.log('최소 salary를 가진 사용자:', min(userList, 'salary'));
 console.log('최소 age를 가진 사용자:', min(userList, 'age'));
@@ -77,6 +87,29 @@ console.log('서울에 사는 첫 번째 사용자:', firstUserInSeoul);
 const hasHighEarningUser = some((user) => user.salary >= 5000000);
 console.log('연봉이 5,000,000 이상인 사용자가 있는가?:', hasHighEarningUser);
 
-// // // 모든 사용자가 '서울'에 사는지 확인하는 예시 every인데 필요없음.
-const allUsersInSeoul = some((user) => user.address === '서울');
+// // // 모든 사용자가 '서울'에 사는지 확인하는 예시
+const allUsersInSeoul = every((user) => user.address === '서울');
 console.log('모든 사용자가 서울에 사는가?:', allUsersInSeoul);
+
+
+//////////
+// min 다른 풀이
+/*
+function min(userList, property) {
+  // userList가 비어있으면 null을 반환
+  if (userList.length === 0) {
+    return null;
+  }
+
+  let minValueUser = userList[0]; // 최소값을 가진 객체를 첫 번째 사용자로 초기화
+
+  // userList를 순회하면서 주어진 속성의 최소값을 찾음
+  for (const user of userList) {
+    if (user[property] < minValueUser[property]) {
+      minValueUser = user;
+    }
+  }
+
+  return minValueUser;
+}
+*/
