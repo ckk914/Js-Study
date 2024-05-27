e93ab97d57567cc92d9dbcf414d3422c75b8ae56
ckk914 <ckk9114@naver.com>
2024-05-27 22:20:21 +0900

    #주석 수정, 비동기 처리 방식 fetch 재정리

--- 3.AJAX/2. Promise/app.js
+++ 3.AJAX/2. Promise/app.js
@@ -27,12 +27,17 @@ const renderPosts = postList => {
     const $template = document.getElementById('single-post');
     // 템플릿 태그에서 li태그 추출
     // 복사 !! true는 깊은 복사 안에 내용 전체 복사
-    const $li = document.importNode($template.content, true);
+    //true 안에 있는 것도 가져온다
+    //false : 태그만 가져옴
+    const $li = document.importNode($template.content, true); 
+    
     // console.log($li);
-    $li.querySelector('h2').textContent = post.title;
+    //li 태그로 가져온 것에서 h2 내용 지정 : 디비(웹)에서 가져온 것을 넣어준다~!
+    $li.querySelector('h2').textContent = post.title;   
     $li.querySelector('p').textContent = post.body;
     //자식에 추가
-    $postUl.appendChild($li);
+    $postUl.appendChild($li);     //ul에 li를 붙여준다~!
+
   });
 };
 
