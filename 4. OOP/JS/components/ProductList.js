5b3708169c4d924f0a822b55a2f820b9bdc2b2ab
ckk914 <ckk9114@naver.com>
2024-06-11 12:15:21 +0900

    #- 코드 모듈화

--- 4. OOP/JS/components/ProductList.js
+++ 4. OOP/JS/components/ProductList.js
@@ -1,27 +1,29 @@
+import Component from "./Component.js";
 import ProductItem from "./ProductItem.js";
-//ul태그 렌더링 담당
-class productList {
-  //배열을 받아옴
-  constructor(products) {
+
+// UL태그 렌더링 담당
+class ProductList extends Component {
+  // JSON형태의 배열을 받아옴
+  constructor(tagId, products) {
+    super(tagId);
     this.products = products;
   }
-  render() {
-    // render: function () == render()
-    const $prodList = document.createElement("ul");
-    $prodList.classList.add("product-list");
-    //반복해서 li 태그를 생성
-    // console.log("products:", this.products);
-    this.products.forEach((prod) => {
-      const product = new ProductItem(prod);
 
-      $prodList.appendChild(product.render()); //마찬가지로 ul에 li가 꽂힌다~!
+  render() {
+    const attr = {
+      id: "prod-list",
+    };
+    this.createElement("ul", "product-list", "", attr);
+
+    // 반복해서 li태그를 생성
+    // console.log('products: ', this.products);
+    this.products.forEach((prod) => {
+      new ProductItem(attr.id, prod).render();
     });
 
-    //div#app에 ul 추가
-    //   document.getElementById("app").appendChild($prodList);
-
-    return $prodList;
+    // div#app에 ul추가
+    // document.getElementById('app').appendChild($prodList);
   }
 }
 
-export default productList;
+export default ProductList;
--- 4. OOP/JS/components/ShoppingCart.js
+++ 4. OOP/JS/components/ShoppingCart.js
@@ -1,18 +1,20 @@
-class ShoppingCart {
-  constructor() {
-    //장바구니에 담은 product들을 저장
+import Component from "./Component.js";
+
+class ShoppingCart extends Component {
+  constructor(tagId) {
+    super(tagId);
+    // 장바구니에 담은 product들을 저장
     this.cartItems = [];
   }
 
   render() {
-    const $header = document.createElement('section');
-    $header.classList.add('cart');
-    $header.innerHTML = `
-        <h2>총액 0원<h2>
-        <button>주문하기</button>
-        `;
-    return $header;
+    const childHtml = `
+      <h2>총액 0원</h2>
+      <button>주문하기</button>
+    `;
+
+    this.createElement("section", "cart", childHtml);
   }
 }
-//외부로 쏨
+
 export default ShoppingCart;
