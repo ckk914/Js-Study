523514fb17467adcc560778ddfd314aa4b75cd70
ckk914 <ckk9114@naver.com>
2024-06-11 12:44:15 +0900

    #총액 계산되도록 수정

--- 4. OOP/JS/components/ProductList.js
+++ 4. OOP/JS/components/ProductList.js
@@ -4,9 +4,10 @@ import ProductItem from "./ProductItem.js";
 // UL태그 렌더링 담당
 class ProductList extends Component {
   // JSON형태의 배열을 받아옴
-  constructor(tagId, products) {
+  constructor(tagId, products,shoppingCart) {
     super(tagId);
     this.products = products;
+    this.shoppingCart = shoppingCart;
   }
 
   render() {
@@ -18,7 +19,8 @@ class ProductList extends Component {
     // 반복해서 li태그를 생성
     // console.log('products: ', this.products);
     this.products.forEach((prod) => {
-      new ProductItem(attr.id, prod).render();
+      new ProductItem(attr.id, prod,
+      this.shoppingCart).render();
     });
 
     // div#app에 ul추가
